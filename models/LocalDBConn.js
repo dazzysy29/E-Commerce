90b253a1c945374d35cbaa405fab0dea4643d01a
dazzysy29 <37176568+dazzysy29@users.noreply.github.com>
2018-03-19 21:59:55 -0500

    改变数据库的授权信息

--- models/LocalDBConn.js
+++ models/LocalDBConn.js
@@ -3,8 +3,8 @@
  */
 let Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('onlineshoppingdb', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('onlineshoppingdb', 'dazzysy@dazzback-mysqldbserver', 'Mima@123', {
+    host: 'dazzback-mysqldbserver.mysql.database.azure.com',
     dialect: 'mysql',
     pool:{
         max: 5,
