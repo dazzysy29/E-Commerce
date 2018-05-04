fc0990edb1e75ed22bfd1c2d5d08122d1aa0aa54
dazzysy29 <37176568+dazzysy29@users.noreply.github.com>
2018-05-03 19:07:06 -0500

    更新数据库服务器地址以及用户名
    旧的azure数据库服务过期，更换成新的azure数据库服务器地址

--- models/LocalDBConn.js
+++ models/LocalDBConn.js
@@ -3,8 +3,8 @@
  */
 let Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('onlineshoppingdb', 'dazzysy@dazzback-mysqldbserver', 'Mima@123', {
-    host: 'dazzback-mysqldbserver.mysql.database.azure.com',
+const sequelize = new Sequelize('onlineshoppingdb', 'dazzysy@dazzecommerceback-mysqldbserver', 'Mima@123', {
+    host: 'dazzecommerceback-mysqldbserver.mysql.database.azure.com',
     dialect: 'mysql',
     pool:{
         max: 5,
